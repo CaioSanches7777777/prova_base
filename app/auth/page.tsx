de90e53cee92a4956b8af1cccaa2af40d8c7c965
Caio Sanches <102621072+CaioSanches7777777@users.noreply.github.com>
2024-06-14 09:55:42 -0300

    Add files via upload

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,33 +1,11 @@
-'use client'
- 
-import { authenticate } from '@/components/Authenticate'
-import { useFormState, useFormStatus } from 'react-dom'
+
  
 export default function Page() {
-  const [errorMessage, dispatch] = useFormState(authenticate, undefined)
- 
   return (
-    <form action={dispatch}>
-      <input type="email" name="email" placeholder="Email" required />
+    <form action={authenticate}>
+      <input type="username" name="username" placeholder="Username" required />
       <input type="password" name="password" placeholder="Password" required />
-      <div>{errorMessage && <p>{errorMessage}</p>}</div>
-      <LoginButton />
+      <button type="submit">Login</button>
     </form>
-  )
-}
- 
-function LoginButton() {
-  const { pending } = useFormStatus()
- 
-  const handleClick = (event) => {
-    if (pending) {
-      event.preventDefault()
-    }
-  }
- 
-  return (
-    <button aria-disabled={pending} type="submit" onClick={handleClick}>
-      Login
-    </button>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Link from "@/node_modules/next/link";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center gap-4 w-full">
       <h1>Bem vindo ao Portal de Receitas</h1>
-      <Link href="/recipes">Receitas</Link>
+      <Link className="bg-gray-600 text-white py-2 px-4 m-2 rounded-lg hover:bg-gray-800" href="/recipes">Receitas</Link>
     </div>
   );
 }
